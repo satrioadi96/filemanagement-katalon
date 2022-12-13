b44bb502fef32faa5b6b5453620e3960adb1aab4
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-12-13 13:36:00 +0700

    input number testcase

--- Scripts/inputNumber/Script1670909986456.groovy
+++ Scripts/inputNumber/Script1670909986456.groovy
@@ -17,3 +17,13 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://the-internet.herokuapp.com/')
+
+WebUI.click(findTestObject('Object Repository/inputNumber/Page_The Internet/a_Inputs'))
+
+WebUI.setText(findTestObject('Object Repository/inputNumber/Page_The Internet/input'), '11')
+
+WebUI.closeBrowser()
+
